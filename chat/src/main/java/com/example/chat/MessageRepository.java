c7f05b7ccbf4994d2eea87f2bf9c8b50d615ca69
ibrahimelbehery <ibrahimomar605@gmail.com>
2023-05-20 22:43:33 +0000

    third commit

--- chat/src/main/java/com/example/chat/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.chat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface MessageRepository extends JpaRepository<Message, Integer> {
-    List<Message> findByUserId(Integer userId);
-}
